1f8231e3f4fce65fc656e7adc58209fedf2528c9
nck4s <nn.2nn@mail.ru>
2024-06-04 13:45:52 +1000

    uppercase and lowercase

--- yo_app/views.py
+++ yo_app/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 def index(request):
     if request.method == 'POST':
         text = request.POST['text']
-        new_text = text.replace('ё', 'е')
+        translation_table = str.maketrans("ёЁ", "еЕ")
+        new_text = text.translate(translation_table)
         return render(request, 'yo_app/index.html', {'new_text': new_text})
     else:
         return render(request, 'yo_app/index.html')
+

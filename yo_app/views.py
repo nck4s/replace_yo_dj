2b39edca6d5d7bb00f33947cfb0d84fc6b34fd99
nck4s <nn.2nn@mail.ru>
2024-06-05 21:59:52 +1000

    added new features

--- yo_app/views.py
+++ yo_app/views.py
@@ -1,11 +1,45 @@
 from django.shortcuts import render
+import re
 
 def index(request):
     if request.method == 'POST':
         text = request.POST['text']
+        
+        # Замена ё на е и Ё на Е
         translation_table = str.maketrans("ёЁ", "еЕ")
-        new_text = text.translate(translation_table)
+        text = text.translate(translation_table)
+        
+        # Удаление всех кавычек
+        text = text.replace("'", "").replace('"', "")
+        
+        # Функция для замены всех заглавных букв на строчные, если слово написано капсом
+        def lower_case_if_caps(word):
+            return word.lower() if word.isupper() else word
+
+        # Функция для обработки одного предложения
+        def process_sentence(sentence):
+            words = sentence.split()
+            processed_words = [lower_case_if_caps(word) for word in words]
+            return ' '.join(processed_words)
+        
+        # Разбиваем текст на предложения
+        sentences = re.split(r'(?<=\.)', text)
+
+        # Обрабатываем каждое предложение и сохраняем заглавные буквы в начале предложений
+        new_sentences = []
+        for sentence in sentences:
+            sentence = sentence.strip()
+            if not sentence:
+                continue
+            # Обрабатываем предложение
+            processed_sentence = process_sentence(sentence)
+            # Восстанавливаем заглавную букву в начале предложения
+            processed_sentence = processed_sentence[0].upper() + processed_sentence[1:]
+            new_sentences.append(processed_sentence)
+        
+        # Объединяем предложения обратно в текст
+        new_text = ' '.join(new_sentences)
+
         return render(request, 'yo_app/index.html', {'new_text': new_text})
     else:
         return render(request, 'yo_app/index.html')
-
